2e3153913ccd6f8c7baeb5a84fbfe1cc701adcba
Sarah Holder <sr.holder@yahoo.com>
2020-07-16 12:48:17 -0500

    added feed to animals

--- exercises/petting_zoo/animals.py
+++ exercises/petting_zoo/animals.py
@@ -2,55 +2,63 @@ from datetime import date
 
 #outdoor corral (walking)
 class Donkey: 
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
       self.name = name
       self.species = species
       self.date_added = date.today()
       self.walking = True
       self.shift = shift
+      self.food = food
+
+    def feed(self):
+      print(f'{self.name} was fed on {date.today().strftime("%m/%d/%Y")}')
       
     def __str__(self):
       return f"This animal is named {self.name}, it is a {self.species} and it was added on {self.date_added} and it is {bool(self.walking)} that it walks and it is available to pet during the {self.shift} shift.\n"
 class Sheep: 
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
       self.name = name
       self.species = species
       self.date_added = date.today()
       self.walking = True
       self.shift = shift
+      self.food = food
       
     def __str__(self):
       return f"This animal is named {self.name}, it is a {self.species} and it was added on {self.date_added} and it is {bool(self.walking)} that it walks and it is available to pet during the {self.shift} shift.\n"
 
 class Goat: 
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
       self.name = name
       self.species = species
       self.date_added = date.today()
       self.walking = True
       self.shift = shift
+      self.food = food
       
     def __str__(self):
       return f"This animal is named {self.name}, it is a {self.species} and it was added on {self.date_added} and it is {bool(self.walking)} that it walks and it is available to pet during the {self.shift} shift.\n"
 
 class Cow: 
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
       self.name = name
       self.species = species
       self.date_added = date.today()
       self.walking = True
       self.shift = shift
+      self.food = food
       
     def __str__(self):
       return f"This animal is named {self.name}, it is a {self.species} and it was added on {self.date_added} and it is {bool(self.walking)} that it walks and it is available to pet during the {self.shift} shift.\n"
 
 class Horse: 
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
       self.name = name
       self.species = species
       self.date_added = date.today()
       self.walking = True
       self.shift = shift
+      self.food = food
 
     def __str__(self):
       return f"This animal is named {self.name}, it is a {self.species} and it was added on {self.date_added} and it is {bool(self.walking)} that it walks and it is available to pet during the {self.shift} shift.\n"
@@ -158,12 +166,11 @@ class KoiFish:
     def __str__(self):
       return f"This animal is named {self.name}, it is a {self.species} and it was added on {self.date_added} and it is {bool(self.swimming)} that it swims.\n"
 
-
-JA = Donkey("JA", "donkey", "morning")
-Black = Sheep("Black", "sheep", "afternoon")
-Billy = Goat("Billy", "goat", "midday")
-Milkshake = Cow("Milkshake", "Cow", "morning")
-RedRum = Horse("RedRum", "horse", "midday")
+JA = Donkey("JA", "donkey", "morning", "straw")
+Black = Sheep("Black", "sheep", "afternoon", "hay")
+Billy = Goat("Billy", "goat", "midday", "pellets")
+Milkshake = Cow("Milkshake", "Cow", "morning", "grass")
+RedRum = Horse("RedRum", "horse", "midday", "apples")
 
 Noodles = Watersnake("Noodles", "watersnake")
 Zoe = Rattlesnake("Zoe", "rattlesnake")
@@ -177,7 +184,7 @@ Batman = BlueFish("Batman", "blue fish")
 Cleo = Catfish("Cleo", "catfish")
 Kiki = KoiFish("Kiki", "koi fish")
 
-print(JA, Black, Billy, Milkshake, RedRum)
+print(JA, JA.feed(), Black, Black.feed(), Billy, Billy.feed(), Milkshake, Milkshake.feed(), RedRum, RedRum.feed())
 print(Noodles, Zoe, Snape, Buttercup, Sssssam)
 print(Goldie, Romeo, Batman, Cleo, Kiki)
 # for prop, value in JA.__dict__.items():
